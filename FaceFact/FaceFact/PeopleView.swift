dfd747729a96aebd9fd83670dac9b30a71eef548
dundi126 <dundi126@gmail.com>
2025-07-25 20:41:04 -0400

    Add user

---
+++ FaceFact/FaceFact/PeopleView.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleView.swift
+//  FaceFact
+//
+//  Created by Dundi vignesh Gutti on 7/25/25.
+//
+
+import SwiftUI
+
+struct PeopleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeopleView()
+}
