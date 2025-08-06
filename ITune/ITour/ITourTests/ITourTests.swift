7e41fc0c3cd5f8e15a9cbb27b02b3ee1c6fc2bb7
dundi126 <dundi126@gmail.com>
2025-08-06 15:42:10 -0400

    add new project Itour and removed all the duplicates

---
+++ ITune/ITour/ITourTests/ITourTests.swift
@@ -0,0 +1,17 @@
+//
+//  ITourTests.swift
+//  ITourTests
+//
+//  Created by Dundi vignesh Gutti on 8/6/25.
+//
+
+import Testing
+@testable import ITour
+
+struct ITourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Swift-AppGround/ITour/ITour/ITourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITourApp.swift
+//  ITour
+//
+//  Created by Dundi vignesh Gutti on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ITourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
